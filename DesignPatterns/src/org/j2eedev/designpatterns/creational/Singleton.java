878604968df371c81a654b875411b9e450680a9e
Umashankar <umashankar@j2eedev.org>
2014-04-21 10:49:04 +0530

    Singleton implementation in Java

---
+++ DesignPatterns/src/org/j2eedev/designpatterns/creational/Singleton.java
@@ -0,0 +1,22 @@
+package org.j2eedev.designpatterns.creational;
+/**
+ * 
+ * @author Umashankar
+ * http://j2eedev.org
+ *
+ */
+public class Singleton {
+  
+  private static Singleton _INSTANCE = new Singleton();
+
+	// Marking default constructor private
+	// to avoid direct instantiation.
+	private Singleton() {
+
+	}
+
+	// Get instance for class Singleton
+	public static Singleton getInstance() {
+		return _INSTANCE;
+	}
+}
