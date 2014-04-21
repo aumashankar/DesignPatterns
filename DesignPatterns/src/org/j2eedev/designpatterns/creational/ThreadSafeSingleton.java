212a07ba601885443d689ceb739baf9413b03522
Umashankar <umashankar@j2eedev.org>
2014-04-21 10:51:49 +0530

    Thread safe Singleton

---
+++ DesignPatterns/src/org/j2eedev/designpatterns/creational/ThreadSafeSingleton.java
@@ -0,0 +1,27 @@
+package org.j2eedev.designpatterns.creational;
+/**
+ * 
+ * @author Umashankar
+ * http://j2eedev.org
+ *
+ */
+public class ThreadSafeSingleton {
+  
+  private static ThreadSafeSingleton _INSTANCE;
+	// Marking default constructor private
+	// to avoid direct instantiation.
+	private ThreadSafeSingleton() {
+
+	}
+	// Get instance for class ThreadSafeSingleton
+	public static synchronized ThreadSafeSingleton getInstance() {
+		if(_INSTANCE==null){
+			_INSTANCE=new ThreadSafeSingleton();
+		}
+		return _INSTANCE;
+	}
+	//clone() can spoil the game! so override it.
+	public Object clone() throws CloneNotSupportedException {
+	      throw new CloneNotSupportedException();
+	}
+}
